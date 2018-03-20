6af5140acb29753fa3aba650360b4ba0ec952fdd
Josh Turgeon <j_turgeon88@yahoo.com>
2018-03-19 19:46:01 -0500

    Phase 3 Root component complete

--- Pokedex/frontend/pokedex.jsx
+++ Pokedex/frontend/pokedex.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 // Test
 import * as APIUtil from './util/api_util';
 import * as PokemonActions from './actions/pokemon_actions';
 import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const rootEl = document.getElementById('root');
   const store = configureStore();
+  const rootEl = document.getElementById('root');
   //FOR TESTING:
-  window.fetchAllPokemon = APIUtil.fetchAllPokemon;
-  window.requestAllPokemon = PokemonActions.requestAllPokemon;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.selectAllPokemon = selectAllPokemon;
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  // window.fetchAllPokemon = APIUtil.fetchAllPokemon;
+  // window.requestAllPokemon = PokemonActions.requestAllPokemon;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.selectAllPokemon = selectAllPokemon;
+  
+  ReactDOM.render(<Root store={store}/>, rootEl);
 });
